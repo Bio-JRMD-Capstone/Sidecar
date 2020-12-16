a854f47e8997a5fea180ccf391b41b5a85445c8a
Michael <michael.c.meyer26@gmail.com>
2020-12-15 22:18:39 -0600

    feat: initial implementation of allowing user to add a POI

--- src/main/java/live/jrmd/sidecar/controllers/POIController.java
+++ src/main/java/live/jrmd/sidecar/controllers/POIController.java
@@ -1,11 +1,14 @@
 package live.jrmd.sidecar.controllers;
 
 import live.jrmd.sidecar.models.POI;
+import live.jrmd.sidecar.models.User;
 import live.jrmd.sidecar.repositories.POIRepository;
 import live.jrmd.sidecar.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -33,8 +36,16 @@ public class POIController {
         return "points/index";
     }
     @GetMapping("/points/add")
-    public String add(Model model){
+    public String add(Model model) {
+        model.addAttribute("poi", new POI());
         model.addAttribute("points", poiDao.findAll());
         return "points/add";
+    }
+    @PostMapping("/points/add")
+    public String create(@ModelAttribute POI poiToBeSaved) {
+        User user = userDao.getOne(1L);
+        poiToBeSaved.setUser(user);
+        POI dbPOI = poiDao.save(poiToBeSaved);
+        return "redirect:/points";
     }
 }
