7848d93114204652e1df1beeda96916f9e2f731a
Jason Carrillo <jason.carrillo182@gmail.com>
2020-12-18 08:23:06 -0700

    commented out broken code, and left only working code for functionaility

--- src/main/resources/static/js/createRoute.js
+++ src/main/resources/static/js/createRoute.js
@@ -102,53 +102,53 @@ function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
 
-    function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-        const waypts = [];
-
-        for (let i = 0; i < markers.length; i++) {
-                waypts.push({
-                    location: markers[i].value,
-                    stopover: true,
-                });
-
-        }
-        directionsService.route(
-            {
-                origin: "El Paso, TX",
-                destination: "Dallas, TX",
-                waypoints: waypts,
-                optimizeWaypoints: true,
-                travelMode: google.maps.TravelMode.DRIVING,
-            },
-            (response, status) => {
-                if (status === "OK") {
-                    directionsRenderer.setDirections(response);
-                    const route = response.routes[0];
-                    const summaryPanel = document.getElementById("directions-panel");
-                    summaryPanel.innerHTML = "";
-
-                    // For each route, display summary information.
-                    for (let i = 0; i < route.legs.length; i++) {
-                        const routeSegment = i + 1;
-                        summaryPanel.innerHTML +=
-                            "<b>Route Segment: " + routeSegment + "</b><br>";
-                        summaryPanel.innerHTML += route.legs[i].start_address + " to ";
-                        summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
-                        summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
-                    }
-                } else {
-                    window.alert("Directions request failed due to " + status);
-                }
-            }
-        );
-    }
+    // function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    //     const waypts = [];
+    //
+    //     for (let i = 0; i < markers.length; i++) {
+    //             waypts.push({
+    //                 location: markers[i],
+    //                 stopover: true,
+    //             });
+    //
+    //     }
+    //     directionsService.route(
+    //         {
+    //             origin: "El Paso, TX",
+    //             destination: "Dallas, TX",
+    //             waypoints: waypts,
+    //             optimizeWaypoints: true,
+    //             travelMode: google.maps.TravelMode.DRIVING,
+    //         },
+    //         (response, status) => {
+    //             if (status === "OK") {
+    //                 directionsRenderer.setDirections(response);
+    //                 const route = response.routes[0];
+    //                 const summaryPanel = document.getElementById("directions-panel");
+    //                 summaryPanel.innerHTML = "";
+    //
+    //                 // For each route, display summary information.
+    //                 for (let i = 0; i < route.legs.length; i++) {
+    //                     const routeSegment = i + 1;
+    //                     summaryPanel.innerHTML +=
+    //                         "<b>Route Segment: " + routeSegment + "</b><br>";
+    //                     summaryPanel.innerHTML += route.legs[i].start_address + " to ";
+    //                     summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+    //                     summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+    //                 }
+    //             } else {
+    //                 window.alert("Directions request failed due to " + status);
+    //             }
+    //         }
+    //     );
+    // }
 
     $(function(){
-        // $( "#testbtn" ).on( 'click', function (){
-        //     calculateAndDisplayRoute();
-        //     initMapRoute();
-        // });
-        $("#testbtn").on('click', calculateAndDisplayRoute)
+        $( "#testbtn" ).on( 'click', function (){
+            // calculateAndDisplayRoute();
+            initMapRoute();
+        });
+        // $("#testbtn").on('click', calculateAndDisplayRoute)
         $( "#clearBtn" ).on( 'click', initMap);
     });
 
