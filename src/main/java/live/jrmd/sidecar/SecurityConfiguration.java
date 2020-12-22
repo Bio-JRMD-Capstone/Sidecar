e9b24023bc3e5b0e0359494dd7c2869e5d247333
Donelle Harris <donelle.harris@gmail.com>
2020-12-22 09:46:45 -0600

    Add ability to create new event, post all events to events index view
    new event eventType selected by radio button
    user may only upload image file

--- src/main/java/live/jrmd/sidecar/SecurityConfiguration.java
+++ src/main/java/live/jrmd/sidecar/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                         "/points",
 //                                        "/points/add",
                                         "/events",
+//                                        "/events/create",
                                         "/js/**",
                                         "/css/**"
                 )
