4efa8763ffa54fd3f309f19643622f17efc9e85c
Donelle Harris <donelle.harris@gmail.com>
2021-01-13 22:45:08 -0600

    Refactor view and controller to allow user to edit photo
    Also set profile image placeholder if none is uploaded

--- src/main/java/live/jrmd/sidecar/controllers/UserController.java
+++ src/main/java/live/jrmd/sidecar/controllers/UserController.java
@@ -58,7 +58,8 @@ public class UserController {
     public String saveUser(@Valid User user,
                            Errors validation,
                            Model model,
-                           @ModelAttribute User newUser
+                           @ModelAttribute User newUser,
+                           @RequestParam(name = "photo_url") String photo_url
     ){
         if(validation.hasErrors() || (!newUser.getPassword().equals(newUser.getPassword_confirm()))){
             model.addAttribute("errors", validation);
@@ -69,6 +70,10 @@ public class UserController {
                 String hash = passwordEncoder.encode(newUser.getPassword());
                 newUser.setPassword(hash);
                 newUser.setPassword_confirm(hash);
+            }
+
+            if(photo_url.equals("")){
+                newUser.setPhoto_url("https://cdn.filestackcontent.com/Ts54QC2PQv6ZToYPspKM");
             }
             users.save(newUser);
             return "redirect:/login";
@@ -94,12 +99,17 @@ public class UserController {
     public String editUser (@PathVariable(value = "id") long id,
                             @RequestParam(name = "username") String username,
                             @RequestParam(name = "email") String email,
-                            @RequestParam(name = "zipcode") String zipcode
+                            @RequestParam(name = "zipcode") String zipcode,
+                            @RequestParam(name = "photo_url") String photo_url
     ){
             User user = (userDao.getUserById(id));
             user.setUsername(username);
             user.setEmail(email);
             user.setZipcode(zipcode);
+            if(photo_url.equals("")){
+                user.setPhoto_url("https://cdn.filestackcontent.com/Ts54QC2PQv6ZToYPspKM");
+            }
+            user.setPhoto_url(photo_url);
             user.setPassword(user.getPassword());
             user.setPassword_confirm(user.getPassword());
 
